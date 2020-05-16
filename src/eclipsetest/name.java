d4bf0a3d25bf5c53373ee7e1f67712069aaedfa5
U-intel-PC\intel <leenap162@gmail.com>
2020-05-16 17:56:59 +0530

    jjj

---
+++ src/eclipsetest/name.java
@@ -0,0 +1,33 @@
+package eclipsetest;
+
+public class name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("N  ");
+		for(int i=0;i<4;i++) {
+			if(i==0||i==3) {
+			for(int j=0;j<6;j++) {
+				System.out.print(" "+"*");
+			}
+			System.out.println("");
+			}
+			else {
+				for(int j=0;j<6;j++) {
+					if(j==0||j==5) {
+						System.out.print(" "+"*");
+					}
+					else {
+						System.out.print("  ");
+					}
+					}
+				System.out.println("");
+				}
+			}
+			
+			
+		}
+
+	}
+
+
